1581f484a9a040fbfcddc4fecec9d1676913ff43
David <dvedvick@gmail.com>
2015-06-19 13:35:56 -0500

    Add initial notes, tweak resume styling slightly

--- views/resume/resume.jsx
+++ views/resume/resume.jsx
@@ -6,7 +6,7 @@ var Resume = React.createClass({
 	render: function() {
 		return (
 			<Layout subheader="Resume">
-				<div className="content" dangerouslySetInnerHTML={{__html: marked((this.props.resume || "").toString(), {sanitize: true})}} />
+				<div className="resume" dangerouslySetInnerHTML={{__html: marked((this.props.resume || "").toString(), {sanitize: true})}} />
 			</Layout>
 		);
 	}
