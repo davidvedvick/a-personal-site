50cf4e5d1980d98638d6bf39c88d271ff928f3a5
david <dvedvick@gmail.com>
2015-07-09 22:28:56 -0500

    Add syntax highlighting to code sections

--- views/notes/note/note.jsx
+++ views/notes/note/note.jsx
@@ -2,19 +2,27 @@ var React = require('react');
 var marked = require('marked');
 var moment = require('moment');
 var path = require('path');
+var highlightJs = require('highlight.js');
 
 var Note = React.createClass({
-	shouldComponentUpdate: function(nextProps, nextState) {
+	shouldComponentUpdate: function (nextProps, nextState) {
 		// After initial rendering, the note never changes, so leave it alone
 		return false;
 	},
-	render: function() {
+	render: function () {
 		var note = this.props.note;
 		var routeUrl = path.join('/notes', note.pathYear, note.pathMonth, note.pathDay, note.pathTitle);
+		var html = marked(note.text || '', {
+			sanitize: true,
+			highlight: function (code) {
+				return highlightJs.highlightAuto(code).value;
+			}
+		});
+
 		return (
 			<div className="note">
 				<h2 className="note-title">{note.title}</h2>
-				<div className="note-text" dangerouslySetInnerHTML={{__html: marked(note.text || "", {sanitize: true})}} />
+				<div className="note-text" dangerouslySetInnerHTML={{__html: html}} />
 				<p className="note-date">
 					<em>Note posted on {moment(note.created).format('LLLL')} - <a href={routeUrl}>link</a></em>
 				</p>
