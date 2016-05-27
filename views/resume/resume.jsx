a447935d946ec411c01682ed99b9f0bf1f0205f4
david <dvedvick@gmail.com>
2016-05-26 21:45:04 -0500

    Make resume functional

--- views/resume/resume.jsx
+++ views/resume/resume.jsx
@@ -1,18 +1,14 @@
-var React = require("react");
-var Layout = require("../layout");
-var marked = require("marked");
+import { div, a, img } from 'react-hyperscript-helpers';
+import Layout from '../layout';
+import marked from 'marked';
 
-var Resume = React.createClass({
-	render: function() {
-		return (
-			<Layout subheader="Resume">
-				<div className="resume">
-					<a href="/resume.pdf" id="pdf-version"><img src="/imgs/file-pdf.svg" alt="View the PDF version!" /></a>
-					<div className="resume-content" dangerouslySetInnerHTML={{__html: marked((this.props.resume || "").toString(), {sanitize: true})}} />
-				</div>
-			</Layout>
-		);
-	}
-});
+var Resume = props => Layout({ subheader: 'Resume' }, [
+	div('.resume', [
+		a('#pdf-version', { href: '/resume.pdf' }, [
+			img({ src: '/imgs/file-pdf.svg', alt: 'View the PDF version!' })
+		]),
+		div('.resume-content', { dangerouslySetInnerHTML: {__html: marked((props.resume || '').toString(), {sanitize: true})} })
+	])
+]);
 
 module.exports = Resume;
