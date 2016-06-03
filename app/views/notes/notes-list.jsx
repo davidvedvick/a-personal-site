fec178255019900c28dc61040143b74ac93ef179
David <dvedvick@gmail.com>
2016-06-03 17:42:48 -0500

    Fix scope of `loadMoreNotesIfNecessary`

--- app/views/notes/notes-list.jsx
+++ app/views/notes/notes-list.jsx
@@ -9,40 +9,37 @@ class NotesList extends React.Component {
 
 		this.page = 1;
 		this.state = { notes: this.props.notes || [] };
-	}
 
-	loadMoreNotesIfNecessary () {
-		if (jQuery(window).scrollTop() >= jQuery('div.note:nth-last-child(5)').offset().top)
-			this.getNotes();
-	}
+		const loadMoreNotesIfNecessary = () => {
+			if (jQuery(window).scrollTop() < jQuery('div.note:nth-last-child(5)').offset().top) return;
 
-	getNotes () {
-		const reactObject = this;
-		jQuery(window).off('scroll', reactObject.loadMoreNotesIfNecessary);
+			jQuery(window).off('scroll', loadMoreNotesIfNecessary);
 
-		jQuery.ajax({
-			url: '/notes/' + (++reactObject.page),
-			dataType: 'json',
-			cache: false,
-			success: (data) => {
-				if (data.length === 0) return;
+			jQuery.ajax({
+				url: '/notes/' + (++this.page),
+				dataType: 'json',
+				cache: false,
+				success: (data) => {
+					if (data.length === 0) return;
 
-				reactObject.setState({notes: reactObject.state.notes.concat(data)});
-				jQuery(window).on('scroll', reactObject.loadMoreNotesIfNecessary);
-			},
-			error: (xhr, status, err) => {
-				console.error(err.toString());
-				jQuery(window).on('scroll', reactObject.loadMoreNotesIfNecessary);
-			}
-		});
+					this.setState({notes: this.state.notes.concat(data)});
+					jQuery(window).on('scroll', loadMoreNotesIfNecessary);
+				},
+				error: (xhr, status, err) => {
+					console.error(err.toString());
+					jQuery(window).on('scroll', loadMoreNotesIfNecessary);
+				}
+			});
+		};
+
+		this.loadMoreNotesIfNecessary = loadMoreNotesIfNecessary;
 	}
 
 	componentDidMount () {
-		const reactObject = this;
 		(($) => {
 			$(() => {
-				$(window).on('scroll', reactObject.loadMoreNotesIfNecessary);
-				reactObject.loadMoreNotesIfNecessary();
+				$(window).on('scroll', this.loadMoreNotesIfNecessary);
+				this.loadMoreNotesIfNecessary();
 			});
 		})(jQuery);
 	}
@@ -50,7 +47,7 @@ class NotesList extends React.Component {
 	render () {
 		// notes objects should look like "{title, date, text}". don't include private
 		// ones
-		var noteNodes = (this.state.notes || [])
+		const noteNodes = (this.state.notes || [])
 			.map((note) => Note({ note: note, key: note.hash }));
 
 		return div(noteNodes);
