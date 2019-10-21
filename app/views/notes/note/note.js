c6ee7f11c5c4dd3000eef10ea091a2096c6663bc
David Vedvick <dvedvick@gmail.com>
2019-10-21 17:40:47 -0500

    Only create renderer once

--- app/views/notes/note/note.js
+++ app/views/notes/note/note.js
@@ -4,27 +4,27 @@ import path from 'path';
 import highlightJs from 'highlight.js';
 import { div, a, em, p, hh } from 'react-hyperscript-helpers';
 
+const renderer = new marked.Renderer();
+renderer.image = (href, title, text) => {
+	if (href.startsWith('./'))
+		href = '/notes/' + href.replace(/^\.\/+/, '');
+
+	let returnTag = `<image src="${href}"`;
+	
+	if (title)
+		returnTag += ` title="${title}"`;
+
+	if (text)
+		returnTag += ` alt="${text}"`;
+	
+	returnTag += '>';
+
+	return returnTag;
+}
+
 const Note = hh((props) => {
 	const note = props.note;
 	const routeUrl = path.join('/notes', note.pathYear, note.pathMonth, note.pathDay, note.pathTitle);
-
-	const renderer = new marked.Renderer();
-	renderer.image = (href, title, text) => {
-		if (href.startsWith('./'))
-			href = '/notes/' + href.replace(/^\.\/+/, '');
-
-		let returnTag = `<image src="${href}"`;
-		
-		if (title)
-			returnTag += ` title="${title}"`;
-
-		if (text)
-			returnTag += ` alt="${text}"`;
-		
-		returnTag += '>';
-
-		return returnTag;
-	}
 
 	const html = marked(note.text || '', {
 		highlight: (code) => highlightJs.highlightAuto(code).value,
