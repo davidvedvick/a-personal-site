0080e32e7bb48c4494bc9c38393d7c1311f35bfc
David Vedvick <dvedvick@gmail.com>
2022-11-28 14:16:41 -0600

    Add Social Media Links to Biography page

--- app/views/index/index.js
+++ app/views/index/index.js
@@ -1,11 +1,15 @@
-import { div, img } from 'react-hyperscript-helpers';
+import { i, div, img, a } from 'react-hyperscript-helpers';
 import LayoutFactory from '../layout';
 import { marked } from 'marked';
 
 const Index = (props) => LayoutFactory([
 	div('.bio', [
 		img('.author-picture', { src: '/imgs/profile-picture.jpg' }),
-		div('.bio-text', { dangerouslySetInnerHTML: { __html: marked((props.bio || '').toString(), { sanitize: true }) } })
+		div('.bio-text', { dangerouslySetInnerHTML: { __html: marked((props.bio || '').toString(), { sanitize: true }) } }),
+		div('.social-links', [
+			a('.linkedin', { href: 'https://www.linkedin.com/in/davidvedvick/' }, [ img({ src: '/imgs/linkedin.svg', title: 'LinkedIn', alt: 'LinkedIn' }) ]),
+			a('.mastodon', { href: 'https://fosstodon.org/@davidvedvick', rel: 'me' }, [ img({ src: '/imgs/mastodon.svg', title: 'Mastodon', alt: 'Mastodon' }) ]),
+		])
 	])
 ]);
 
