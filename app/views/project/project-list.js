86fbb03b8d33f99278d8c7e700500e7e8b1969a4
David <dvedvick@gmail.com>
2023-10-08 13:18:27 -0500

    Show projects with examples first

--- app/views/project/project-list.js
+++ app/views/project/project-list.js
@@ -1,6 +1,10 @@
-import { script, hh } from 'react-hyperscript-helpers';
+import {hh, script} from 'react-hyperscript-helpers';
 import ProjectDetails from './project-details/project-details';
 import Layout from '../layout';
+
+function boundedToOne(number) {
+  return Math.max(Math.min(number, 1), -1);
+}
 
 const Index = hh((props) =>
 	Layout(
@@ -9,6 +13,12 @@ const Index = hh((props) =>
 		.concat(
 			props
 			.projects
+      .sort((a, b) => {
+        const examplesFirst = boundedToOne(b.examples.length) - boundedToOne(a.examples.length);
+        if (examplesFirst !== 0) return examplesFirst;
+
+        return a.body.localeCompare(b.body);
+      })
 			.map((project) => ProjectDetails({ project: project })))));
 
 module.exports = Index;
