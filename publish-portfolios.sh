f8e387041c4daa07435c6e20f3056aa9e1fda583
David Vedvick <dvedvick@gmail.com>
2023-10-15 21:40:03 -0500

    Mount SSH directory for passthrough

--- publish-portfolios.sh
+++ publish-portfolios.sh
@@ -3,6 +3,7 @@
 docker compose build && docker compose run --rm \
   -v "$(pwd)":/src/site \
   -v "$(realpath "${PROJECTS_LOCATION}")":/projects -e PROJECTS_LOCATION=/projects \
+  -v "$(realpath ~/.ssh)":/home/root/.ssh:ro \
   -w /src/site -u "$(id -u)":"$(id -g)" \
   npm run publish-projects
 
