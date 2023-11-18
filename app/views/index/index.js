35216a25c3fc28abd781321c6eb02f039e8404aa
David <dvedvick@gmail.com>
2023-11-18 08:38:34 -0600

    Remove usage of sanitize parameter in marked

--- app/views/index/index.js
+++ app/views/index/index.js
@@ -3,11 +3,12 @@ const {  div, img } = pkg;
 
 import LayoutFactory from '../layout.js';
 import { marked } from 'marked';
+import sanitizeHtml from 'sanitize-html';
 
 const Index = (props) => LayoutFactory([
 	div('.bio', [
 		img('.author-picture', { src: '/imgs/profile-picture.jpg' }),
-		div('.bio-text', { dangerouslySetInnerHTML: { __html: marked((props.bio || '').toString(), { sanitize: true }) } })
+		div('.bio-text', { dangerouslySetInnerHTML: { __html: sanitizeHtml(marked((props.bio || '').toString())) } })
 	])
 ]);
 
