2fbe89709377ee7fa17b7e915f75c50b533ac952
David <dvedvick@gmail.com>
2024-01-28 12:12:11 -0600

    Upgrade marked, sanitize pre-marked markdown

---
+++ app/views/markdown/configured-sanitizer.js
@@ -0,0 +1,5 @@
+import sanitizeHtml from "sanitize-html";
+
+export function sanitize(html) {
+  return sanitizeHtml(html, { allowedClasses: ['hljs*', 'language-*'] });
+}
--- app/views/project/project-details/features/features.js
+++ app/views/project/project-details/features/features.js
@@ -1,20 +1,18 @@
-import { marked } from 'marked';
-import highlightJs from "highlight.js";
+import {Marked} from 'marked';
 import sanitizeHtml from 'sanitize-html';
 import pkg from 'react-hyperscript-helpers';
+import {configuredMarkedHighlighter} from "../../../markdown/configured-marked-highlighter.js";
+import {sanitize} from "../../../markdown/configured-sanitizer.js";
 const { div, hh } = pkg;
+
+const marked = new Marked(configuredMarkedHighlighter);
 
 export default hh((props) =>
 	div(
     '.features',
     {
       dangerouslySetInnerHTML: {
-        __html: sanitizeHtml(marked(
-          props.features || '',
-          {
-            highlight: (code) => highlightJs.highlightAuto(code).value,
-          }
-        ))
+        __html: marked.parse(sanitize(props.features || ''))
       }
     }
   )
