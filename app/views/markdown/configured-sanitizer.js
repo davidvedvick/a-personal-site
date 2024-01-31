246a1ad22b2bf404091d1b1f5f87cae45b7d23f5
David <dvedvick@gmail.com>
2024-01-31 09:22:46 -0600

    Allow img in sanitized HTML

--- app/views/markdown/configured-sanitizer.js
+++ app/views/markdown/configured-sanitizer.js
@@ -1,5 +1,10 @@
 import sanitizeHtml from "sanitize-html";
 
 export function sanitize(html) {
-  return sanitizeHtml(html, { allowedClasses: { '*': ['hljs*', 'language-*'] } });
+  return sanitizeHtml(html, {
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img' ]),
+    allowedClasses: {
+      '*': ['hljs*', 'language-*']
+    }
+  });
 }
