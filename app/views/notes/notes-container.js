8d50839b5d884cf3a4199c804b0cc812c540a3e0
David <dvedvick@gmail.com>
2025-06-22 09:28:07 -0500

    Added link to RSS feed

--- app/views/notes/notes-container.js
+++ app/views/notes/notes-container.js
@@ -2,13 +2,18 @@ import LayoutFactory from '../layout.js';
 import {NotesList} from './notes-list.js';
 import pkg from 'react-hyperscript-helpers';
 
-const { div, script, hh } = pkg;
+const { div, script, hh, a, img } = pkg;
 
 const NotesContainer = hh((props) => {
 	return LayoutFactory(
     { subheader: "Notes" },
     [
-      div({ id: 'notes-container' }, [ NotesList({ notes: props.notes }) ]),
+      div('#rss-container', [
+        a('#rss-link', { href: '/notes/rss.xml' }, [
+          img({ src: '/imgs/rss.svg', alt: 'Get the Feed!' })
+        ]),
+      ]),
+      div({ id: 'notes-container' }, [ NotesList(props) ]),
       script({ type: 'text/javascript', src: '/js/notes.client.js', async: 'async' })
     ]
   );
